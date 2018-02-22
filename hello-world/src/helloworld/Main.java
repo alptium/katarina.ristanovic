786b1118123a2ead8d236e667552153a401a3754
KatarinaR <30474356+KatarinaR@users.noreply.github.com>
2018-02-22 23:24:04 +0100

    Created the Hello World project

--- hello-world/src/helloworld/Main.java
+++ hello-world/src/helloworld/Main.java
@@ -1,10 +1,9 @@
-package hello-world;
+package helloworld;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
-		System.out.println("My name is Katarina");
-		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+		System.out.println("My name is Katarina.");
 	}
 }
