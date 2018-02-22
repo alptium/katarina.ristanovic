223cf20eca5950ec5a16399c9679ee993f5e7fe7
KatarinaR <30474356+KatarinaR@users.noreply.github.com>
2018-02-22 23:53:19 +0100

    Gradebook chnaged

--- Gradebook/src/gradebook/Main.java
+++ Gradebook/src/gradebook/Main.java
@@ -1,6 +1,8 @@
+package gradebook;
+
 import java.util.Scanner;
 
-public class gradeBook {
+public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -9,7 +11,6 @@ public class gradeBook {
 			
 			int sum = 0;
 			
-			
 			String studentName1 = "Marija";
 			String studentLastName1 = "Jovic";
 			System.out.println("Enter a grade " + studentName1 + " " + studentLastName1);
@@ -17,68 +18,54 @@ public class gradeBook {
 			
 			String studentName2 = "Marko";
 			String studentLastName2 = "Petrovic";
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 			System.out.println("Unesite ocenu " + studentName2 + " " + studentLastName2);
-=======
+
 			System.out.println("Enter a grade " + studentName2 + " " + studentLastName2);
->>>>>>> ad1a058688972d2a6a95f4bd1c8f931faf9a4dc8
-=======
+
 			System.out.println("Enter a grade " + studentName2 + " " + studentLastName2);
->>>>>>> ad1a058688972d2a6a95f4bd1c8f931faf9a4dc8
+
 			int grade2 = sc.nextInt();
 			
-			String studentName3= "Ivana";
+			String studentName3 = "Ivana";
 			String studentLastName3 = "Krstic";
-<<<<<<< HEAD
-<<<<<<< HEAD
-			System.out.println("Unesite ocenu  " + studentName3 + " " + studentLastName3 );
-=======
+
 			System.out.println("Enter a grade " + studentName3 + " " + studentLastName3 );
->>>>>>> ad1a058688972d2a6a95f4bd1c8f931faf9a4dc8
-=======
+			
 			System.out.println("Enter a grade " + studentName3 + " " + studentLastName3 );
->>>>>>> ad1a058688972d2a6a95f4bd1c8f931faf9a4dc8
+
+			System.out.println("Enter a grade " + studentName3 + " " + studentLastName3 );
+
 			int grade3 = sc.nextInt();
 			
 			String studentName4 = "Nenad";
 			String studentLastName4 = "Colic";
-<<<<<<< HEAD
-<<<<<<< HEAD
-			System.out.println("Unesite ocenu " + studentName4 + " " + studentLastName4 );
-=======
+
 			System.out.println("Enter a grade " + studentName4 + " " + studentLastName4 );
->>>>>>> ad1a058688972d2a6a95f4bd1c8f931faf9a4dc8
-=======
+
 			System.out.println("Enter a grade " + studentName4 + " " + studentLastName4 );
->>>>>>> ad1a058688972d2a6a95f4bd1c8f931faf9a4dc8
+
+			System.out.println("Enter a grade " + studentName4 + " " + studentLastName4 );
+
 			int grade4 = sc.nextInt();
 			
 			String studentName5 = "Jelena";
 			String studentLastName5 = "Joksimovic";
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 			System.out.println("Unesite ocenu " + studentName5 + " " + studentLastName5);
-=======
 			System.out.println("Enter a grade " + studentName5 + " " + studentLastName5);
->>>>>>> ad1a058688972d2a6a95f4bd1c8f931faf9a4dc8
-=======
+
+
 			System.out.println("Enter a grade " + studentName5 + " " + studentLastName5);
->>>>>>> ad1a058688972d2a6a95f4bd1c8f931faf9a4dc8
+
 			int grade5 = sc.nextInt();
 			
-			
-			
 			sum  = grade1 + grade2 + grade3 + grade4 + grade5;
-			
-		
-			
 			
 			System.out.println("Ms Jamse sum of grade is : " +  sum );
 			
 		}
-
-
 	}
-
 }
+// I try to change everything I undersud but I am not sure that it is all ok.
+// For example what should I do for las comment about initialization of variables
